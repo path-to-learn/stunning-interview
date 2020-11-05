37efe666d1fb25d6343ead8dbe05216d88520762
Prashant Dathwal <pdathwal@atlassian.com>
2020-11-05 09:52:54 -0800

    Using Priority Queue

--- src/by_topic/Queues/MinMeetingRoomRequired.java
+++ src/by_topic/Queues/MinMeetingRoomRequired.java
@@ -1,4 +1,33 @@
 package by_topic.Queues;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 public class MinMeetingRoomRequired {
+    public int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, (o1, o2) -> o1[0] - o2[0]);
+        // room whose end time is less, should be at the top of pqueue
+        // min pq queue based on end time
+        PriorityQueue<int[]> result = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+        for (int[] interval : intervals) {
+            if (!result.isEmpty() && interval[0] >= result.peek()[1]){
+                result.poll();
+            }
+            result.add(interval);
+        }
+        return result.size();
+    }
+
+    @Test
+    public void testMeetingRoom() {
+        // min meeting room required
+        int[][] interval1 = {{9,10}, {4,9}};
+        Assert.assertEquals(1, minMeetingRooms(interval1));
+
+        int[][] interval2 = {{9,10}, {4,9}, {2,5}};
+        Assert.assertEquals(2, minMeetingRooms(interval2));
+    }
 }
--- src/by_topic/Queues/TopFrequentKElements.java
+++ src/by_topic/Queues/TopFrequentKElements.java
@@ -1,4 +1,37 @@
 package by_topic.Queues;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 public class TopFrequentKElements {
+
+    @Test
+    public void testTopKFrequent() {
+        int[] nums = {1,2,3,1,2,3,5,6,7, 7,7,7};
+        System.out.println(topKFrequent(nums, 3));
+    }
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer> resultList = new ArrayList<>();
+        PriorityQueue<Map.Entry<Integer, Integer>> pq =
+                new PriorityQueue<>((o1, o2) -> o2.getValue() - o1.getValue()); // maxheap
+        //populatemap
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i=0; i<nums.length;i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        map.entrySet().forEach(x-> {
+            pq.add(x);
+        });
+
+        for (int i=0; i<k; i++) {
+            resultList.add(pq.poll().getKey());
+        }
+        return resultList;
+    }
 }
