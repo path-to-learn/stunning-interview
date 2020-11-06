b367ded9f68e8ade5e7a8a49088145196af73ee9
Prashant Dathwal <pdathwal@atlassian.com>
2020-11-06 10:01:32 -0800

    fix compilation unit

--- src/by_topic/concurrency/ExecuterStaticTest.java
+++ src/by_topic/concurrency/ExecuterStaticTest.java
@@ -12,7 +12,7 @@ public class ExecuterStaticTest {
   public static void main(String[] args) throws InterruptedException {
     ExecutorService executor = Executors.newFixedThreadPool(3);
     for (int i = 0; i < 5; i++) {
-      Runnable worker = new thread.WorkerThread("" + i);
+      Runnable worker = new WorkerThread("" + i);
       Thread.sleep(1000);
       executor.execute(worker);
     }
@@ -20,7 +20,7 @@ public class ExecuterStaticTest {
     while (!executor.isTerminated()) {
       // .out.println("Finishing the threads...");
     }
-    System.out.println("Map size finally is: " + thread.WorkerThread.map.size());
+    System.out.println("Map size finally is: " + WorkerThread.map.size());
     System.out.println("Finsihed all threads");
   }
 
